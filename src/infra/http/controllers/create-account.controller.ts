027254339477eb1f6be3e18495045197accce0c5
Leonardo <leonardo.everton@inatel.br>
2025-04-12 18:58:05 -0300

    refactor: create account

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,15 +1,8 @@
-import {
-  Body,
-  ConflictException,
-  Controller,
-  HttpCode,
-  Post,
-  UsePipes,
-} from '@nestjs/common'
-import { hash } from 'bcryptjs'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
-import { ZodValidationPipe } from '@/infra/http/zod-validation-pipe/zod-validation-pipe.pipe'
-import z from 'zod'
+import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common'
+
+import { z } from 'zod'
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
+import { ZodValidationPipe } from '../zod-validation-pipe/zod-validation-pipe.pipe'
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -21,7 +14,7 @@ type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly registerStudent: RegisterStudentUseCase) {}
 
   @Post()
   @HttpCode(201)
@@ -29,25 +22,14 @@ export class CreateAccountController {
   async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
 
-    const userWithSameEmail = await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
+    const result = await this.registerStudent.execute({
+      name,
+      email,
+      password,
     })
 
-    if (userWithSameEmail) {
-      throw new ConflictException(
-        'User with same e-mail address already exists.',
-      )
+    if (result.isLeft()) {
+      throw new Error()
     }
-    const hashedPassword = await hash(password, 8)
-
-    await this.prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-      },
-    })
   }
 }
