4c4d571782a9841de584bfc324128b144570435e
Leonardo <leonardo.everton@inatel.br>
2025-04-13 08:56:42 -0300

    feat: create controller edit question

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -12,6 +12,8 @@ import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
+import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
+import { EditQuestionController } from './controllers/edit-question.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -21,6 +23,7 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     CreateQuestionController,
     FetchRecentQuestionsController,
     GetQuestionBySlugController,
+    EditQuestionController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -28,6 +31,7 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     RegisterStudentUseCase,
     AuthenticateStudentUseCase,
     GetQuestionBySlugUseCase,
+    EditQuestionUseCase,
   ],
 })
 export class HttpModule {}
