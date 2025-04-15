ba014d56554aed01e0b798632c6130b8dd6de302
Leonardo <leonardo.everton@inatel.br>
2025-04-15 09:03:07 -0300

    feat: register domain events

--- src/domain/notification/application/subscribers/on-answer-created.ts
+++ src/domain/notification/application/subscribers/on-answer-created.ts
@@ -1,7 +1,9 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import { EventHandler } from '@/core/events/event-handler'
 import { AnswerCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
+import { Injectable } from '@nestjs/common'
 
+@Injectable()
 export class OnAnswerCreated implements EventHandler {
   constructor() {
     this.setupSubscriptions()
